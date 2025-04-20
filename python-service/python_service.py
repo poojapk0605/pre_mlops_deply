03a9cfac013b2180bae0258a35bc7a601a5c7295
Pooj0605 <kanna.p@northeastern.edu>
2025-04-20 11:27:32 -0400

    frontend and model ready9

--- python-service/python_service.py
+++ python-service/python_service.py
@@ -88,5 +88,5 @@ def healthcheck():
 
 if __name__ == '__main__':
     # Run the Flask app on port 5001 (different from Node.js)
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+   port = int(os.environ.get("PORT", 8080))
+   app.run(host='0.0.0.0', port=port)
